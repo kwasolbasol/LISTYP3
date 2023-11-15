8fe104c56a92824e114e35394b71944bba874634
kwasolbasol <kajkoszek666@gmail.com>
2023-11-15 10:12:05 +0100

    nieskonczone bindingi jeszcze/kolkokrzyzyk

--- LISTAJEDEN/MainWindow.xaml.cs
+++ LISTAJEDEN/MainWindow.xaml.cs
@@ -12,134 +12,108 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 namespace LISTAJEDEN
-{ 
+{
     enum Operation
-{
-    none = 0, // brak operacji
-    addition, // dodawanie
-    subtraction, // odejmowanie
-    multiplication, // mnożenie
-    division, // dzielenie
-    result // wynik
-}
-
-public partial class MainWindow : Window
-{
-    private Operation m_eLastOperationSelected = Operation.none;
-      private KolkoKrzyzyk kolkoKrzyzykWindow;
-      public MainWindow()
+    {
+        none = 0,
+        addition,
+        subtraction,
+        multiplication,
+        division,
+        result
+    }
+    public partial class MainWindow : Window
+    {
+        private Operation m_eLastOperationSelected = Operation.none;
+        private KolkoKrzyzyk kolkoKrzyzykWindow;
+        public MainWindow()
         {
             InitializeComponent();
             kolkoKrzyzykWindow = new KolkoKrzyzyk();
             kolkoKrzyzykWindow.Show();
         }
-
-    private void NumberButton_Click(object oSender, RoutedEventArgs eRoutedEventArgs)
-    {
-        if (Operation.result == m_eLastOperationSelected)
+        private void NumberButton_Click(object oSender, RoutedEventArgs eRoutedEventArgs)
+        {
+            if (Operation.result == m_eLastOperationSelected)
+            {
+                txtDisplay.Text = string.Empty;
+                m_eLastOperationSelected = Operation.none;
+            }
+            Button oButton = (Button)oSender;
+            txtDisplay.Text += oButton.Content;
+        }
+        private void txtDisplayMemory_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+        private void EraseButton_Click(object sender, RoutedEventArgs e)
         {
             txtDisplay.Text = string.Empty;
             m_eLastOperationSelected = Operation.none;
         }
-        Button oButton = (Button)oSender;
-        txtDisplay.Text += oButton.Content;
-    }
-
-    private void txtDisplayMemory_TextChanged(object sender, TextChangedEventArgs e)
-    {
-
-    }
-
-    private void EraseButton_Click(object sender, RoutedEventArgs e)
-    {
-        txtDisplay.Text = string.Empty;
-        m_eLastOperationSelected = Operation.none;
-    }
-
-
-
-
-    private void OperationButton_Click(object oSender, RoutedEventArgs eRoutedEventArgs)
-    {
-        // sprawdzenie czy poprzednia operacja czy jest rozna od none i od wyniku, jeśli nie to wykonać pozostałe operacje
-        if ((Operation.none != m_eLastOperationSelected) || (Operation.result != m_eLastOperationSelected))
-        {
-            ResultButton_Click(this, eRoutedEventArgs);
+        private void OperationButton_Click(object oSender, RoutedEventArgs eRoutedEventArgs)
+        {           
+            {
+                ResultButton_Click(this, eRoutedEventArgs);
+            }
+            Button oButton = (Button)oSender;
+            switch (oButton.Content.ToString())
+            {
+                case "+":
+                    m_eLastOperationSelected = Operation.addition;
+                    break;
+                case "-":
+                    m_eLastOperationSelected = Operation.subtraction;
+                    break;
+                case "*":
+                    m_eLastOperationSelected = Operation.multiplication;
+                    break;
+                case "/":
+                    m_eLastOperationSelected = Operation.division;
+                    break;
+                default:
+                    MessageBox.Show("Nieznana operacja!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+            }
+            txtDisplayMemory.Text = txtDisplay.Text;
+            txtDisplay.Text = string.Empty;
         }
-        Button oButton = (Button)oSender;
-        switch (oButton.Content.ToString())
+        private void ResultButton_Click(object sender, RoutedEventArgs e)
         {
-            case "+":
-                m_eLastOperationSelected = Operation.addition;
-                break;
-            case "-":
-                m_eLastOperationSelected = Operation.subtraction;
-
-                break;
-            case "*":
-                m_eLastOperationSelected = Operation.multiplication;
-                break;
-            case "/":
-                m_eLastOperationSelected = Operation.division;
-
-                break;
-            default:
-                MessageBox.Show("Nieznana operacja!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
-
-
+            if ((Operation.result == m_eLastOperationSelected) ||
+        (Operation.none == m_eLastOperationSelected))
+            {
                 return;
-
+            }
+            if (string.IsNullOrEmpty(txtDisplay.Text))
+            {
+                txtDisplay.Text = "0";
+            }
+            switch (m_eLastOperationSelected)
+            {
+                case Operation.addition:
+                    txtDisplay.Text = (double.Parse(txtDisplayMemory.Text) +
+                        double.Parse(txtDisplay.Text)).ToString();
+                    break;
+                case Operation.subtraction:
+                    txtDisplay.Text = (double.Parse(txtDisplayMemory.Text) -
+                        double.Parse(txtDisplay.Text)).ToString();
+                    break;
+                case Operation.multiplication:
+                    txtDisplay.Text = (double.Parse(txtDisplayMemory.Text) *
+                        double.Parse(txtDisplay.Text)).ToString();
+                    break;
+                case Operation.division:
+                    txtDisplay.Text = (double.Parse(txtDisplayMemory.Text) /
+                        double.Parse(txtDisplay.Text)).ToString();
+                    break;
+            }
+            m_eLastOperationSelected = Operation.result;
+            txtDisplayMemory.Text = string.Empty;
         }
-        txtDisplayMemory.Text = txtDisplay.Text;
-
-        txtDisplay.Text = string.Empty;
-
-
-
-    }
-
-    private void ResultButton_Click(object sender, RoutedEventArgs e)
-    {
-        if ((Operation.result == m_eLastOperationSelected) ||
-    (Operation.none == m_eLastOperationSelected))
+        private void txtDisplay_TextChanged(object sender, TextChangedEventArgs e)
         {
-            return;
         }
-        if (string.IsNullOrEmpty(txtDisplay.Text))
-        {
-            txtDisplay.Text = "0";
-        }
-        switch (m_eLastOperationSelected)
-        {
-            case Operation.addition:
-                txtDisplay.Text = (double.Parse(txtDisplayMemory.Text) +
-                    double.Parse(txtDisplay.Text)).ToString();
-                break;
-            case Operation.subtraction:
-                txtDisplay.Text = (double.Parse(txtDisplayMemory.Text) -
-                    double.Parse(txtDisplay.Text)).ToString();
-
-                break;
-            case Operation.multiplication:
-                txtDisplay.Text = (double.Parse(txtDisplayMemory.Text) *
-                    double.Parse(txtDisplay.Text)).ToString();
-                break;
-            case Operation.division:
-                txtDisplay.Text = (double.Parse(txtDisplayMemory.Text) /
-                    double.Parse(txtDisplay.Text)).ToString();
-                break;
-        }
-        m_eLastOperationSelected = Operation.result;
-
-        txtDisplayMemory.Text = string.Empty;
     }
-
-    private void txtDisplay_TextChanged(object sender, TextChangedEventArgs e)
-    {
-
-    }
-}
 }
 
